2c9fb8f7dea09be1b58cd22339f07a9847bc92b4
parkjinuk09 <parkjinuk@gmail.com>
2026-03-25 16:38:01 +0900

    first commit

---
+++ src/main/java/com/vive/gaobackend/GaobackendApplication.java
@@ -0,0 +1,14 @@
+package com.vive.gaobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaobackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaobackendApplication.class, args);
+    }
+
+}
+//first commit
---
+++ src/test/java/com/vive/gaobackend/GaobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vive.gaobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaobackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
