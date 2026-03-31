701a544e671ec6a45bce735071f7eda375224c2c
박진욱 <parkjinuk09@gmail.com>
2026-04-01 00:55:37 +0900

    유저 엔티티(테이블) 70% 완성

--- src/main/java/com/capteam/gaobackend/entity/User.java
+++ src/main/java/com/capteam/gaobackend/entity/User.java
@@ -1,4 +1,47 @@
 package com.capteam.gaobackend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @Column(nullable = false)
+    private String user_id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String department;  //역할
+
+    private String skill;   //스택
+
+    private String experience;  //경험
+
+    private String profileImage;    //프로필 이미지
+
+    private Boolean isLogin;    //로그인 한번 되있는지
+
+    private String user_analyze;    //학생 점수
+
+
+
+
+
+
+
+
 }
