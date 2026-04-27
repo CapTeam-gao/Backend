cb7a07ec09776c49fbc3aff39ce93ba19a4bb6f0
박진욱 <parkjinuk09@gmail.com>
2026-04-27 09:16:46 +0900

    유저 레포짓토리

--- src/main/java/com/capteam/gaobackend/repository/UserRepository.java
+++ src/main/java/com/capteam/gaobackend/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.capteam.gaobackend.repository;
 
-public interface UserRepository {
+import com.capteam.gaobackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,String> {
+    Optional<User> findByStuId(String stuId); //학번으로 있는지 없는지
 }
