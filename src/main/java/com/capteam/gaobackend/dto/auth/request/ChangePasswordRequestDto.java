8d2ed2a38f7526b8385b29d8d082d1d9a93391e6
박진욱 <parkjinuk09@gmail.com>
2026-05-18 12:15:26 +0900

    비번 변경 디티오

--- src/main/java/com/capteam/gaobackend/dto/auth/request/ChangePasswordRequestDto.java
+++ src/main/java/com/capteam/gaobackend/dto/auth/request/ChangePasswordRequestDto.java
@@ -1,4 +1,23 @@
 package com.capteam.gaobackend.dto.auth.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ChangePasswordRequestDto {
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String newPassword;
+
+    @NotBlank
+    private String checkPassword;
 }
