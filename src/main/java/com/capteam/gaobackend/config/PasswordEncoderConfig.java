c3299a13b9dc83046b60e2d0924ce0b6d9a42da9
박진욱 <parkjinuk09@gmail.com>
2026-05-18 12:13:08 +0900

    PasswordEncoder 설정

--- src/main/java/com/capteam/gaobackend/config/PasswordEncoderConfig.java
+++ src/main/java/com/capteam/gaobackend/config/PasswordEncoderConfig.java
@@ -3,6 +3,7 @@ package com.capteam.gaobackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,6 +12,6 @@ public class PasswordEncoderConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
