76df266864760535ebaecd0d4d7c960aaf7678d9
박진욱 <parkjinuk09@gmail.com>
2026-05-22 11:19:55 +0900

    feat: token 및 role 응답 디티오 추가

---
+++ src/main/java/com/capteam/gaobackend/dto/auth/response/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.capteam.gaobackend.dto.auth.response;
+
+import lombok.Builder;
+
+@Builder
+public record AuthResponse(
+        String accessToken,
+        String refreshToken,
+        String role
+) {}
