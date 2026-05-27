4639da2e5283e47ca533b40e3f4350218667cde7
박진욱 <parkjinuk09@gmail.com>
2026-05-27 19:49:20 +0900

    feat: Jpa 추가

--- src/main/java/com/capteam/gaobackend/repository/TeamRepository.java
+++ src/main/java/com/capteam/gaobackend/repository/TeamRepository.java
@@ -1,4 +1,7 @@
 package com.capteam.gaobackend.repository;
 
-public interface TeamRepository {
+import com.capteam.gaobackend.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team,Long> {
 }
