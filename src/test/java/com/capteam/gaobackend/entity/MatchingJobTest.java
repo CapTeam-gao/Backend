4f1b8446d09297edfb4fbb1797c966579b2a7929
박진욱 <parkjinuk09@gmail.com>
2026-06-15 09:38:31 +0900

    test: 매칭 작업 상태 전이 규칙 검증

---
+++ src/test/java/com/capteam/gaobackend/entity/MatchingJobTest.java
@@ -0,0 +1,44 @@
+package com.capteam.gaobackend.entity;
+
+import com.capteam.gaobackend.enums.Grade;
+import com.capteam.gaobackend.enums.MatchingJobStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MatchingJobTest {
+
+    @Test
+    void runningJobCanBeCancelledBeforePersistenceStarts() {
+        MatchingJob job = new MatchingJob("job-id", Grade.GRADE_2);
+
+        assertThat(job.start()).isTrue();
+        assertThat(job.cancel()).isTrue();
+        assertThat(job.getStatus()).isEqualTo(MatchingJobStatus.CANCELLED);
+        assertThat(job.beginCompletion()).isFalse();
+    }
+
+    @Test
+    void completingJobCannotBeCancelled() {
+        MatchingJob job = new MatchingJob("job-id", Grade.GRADE_2);
+
+        job.start();
+        assertThat(job.beginCompletion()).isTrue();
+        assertThat(job.cancel()).isFalse();
+        job.succeed();
+
+        assertThat(job.getStatus()).isEqualTo(MatchingJobStatus.SUCCEEDED);
+    }
+
+    @Test
+    void cancelledJobIsNotChangedToFailed() {
+        MatchingJob job = new MatchingJob("job-id", Grade.GRADE_3);
+
+        job.start();
+        job.cancel();
+        job.fail("failure");
+
+        assertThat(job.getStatus()).isEqualTo(MatchingJobStatus.CANCELLED);
+        assertThat(job.getErrorMessage()).isNull();
+    }
+}
