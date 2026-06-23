fe5dbc3029c79c659719359185c12c81d82be9e4
박진욱 <parkjinuk09@gmail.com>
2026-06-23 13:53:35 +0900

    fix: S3 클라이언트 HTTP 의존성 충돌 해결

--- src/main/java/com/capteam/gaobackend/config/S3Config.java
+++ src/main/java/com/capteam/gaobackend/config/S3Config.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -23,6 +24,7 @@ public class S3Config {
         return S3Client.builder()
                 .region(Region.of(properties.region()))
                 .credentialsProvider(credentialsProvider)
+                .httpClientBuilder(UrlConnectionHttpClient.builder())
                 .build();
     }
 
