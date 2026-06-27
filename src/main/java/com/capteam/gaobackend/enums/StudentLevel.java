3a27c3e648ade42cdee1a25ca29f9aaca69650a8
jae1 <t01046993182@gmail.com>
2026-06-28 01:49:04 +0900

    fix: 학생 등급 enum 추가

--- src/main/java/com/capteam/gaobackend/enums/StudentLevel.java
+++ src/main/java/com/capteam/gaobackend/enums/StudentLevel.java
@@ -1,7 +1,9 @@
 package com.capteam.gaobackend.enums;
 
 public enum StudentLevel {
-    UPPER, //상
-    MIDDLE,//중
-    LOWER//하
+    UPPER,
+    UPPER_MIDDLE,
+    MIDDLE,
+    LOWER_MIDDLE,
+    LOWER
 }
